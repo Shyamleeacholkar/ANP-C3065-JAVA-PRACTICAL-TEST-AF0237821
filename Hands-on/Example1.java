0621fde7cf078bdb6c4eb3efa190df2c9607aa02
SHYAMLEE ACHOLKAR <116187124+Shyamleeacholkar@users.noreply.github.com>
2023-01-25 15:18:31 +0530

    Add files via upload

---
+++ Hands-on/Example1.java
@@ -0,0 +1,28 @@
+//Write a Program to print any multiplication table. 
+
+package java.example1;
+
+public class Example1 extends Thread
+{
+	public void run()
+	{
+		try
+		{
+			for(int i = 1 ; i <= 10 ; i++)
+			{
+				Thread.sleep(1000);	// sleep method of Thread Class
+				System.out.println(i*7);
+			}
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Example1 table = new Example1();
+		table.start();
+	}
+
+}
